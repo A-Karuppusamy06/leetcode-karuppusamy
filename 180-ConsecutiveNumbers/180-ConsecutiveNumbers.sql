0d69550d337cf96beb753ffdba5921bfe7c1b131
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-08-11 18:36:12 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,8 @@
+-- Last updated: 8/11/2026, 6:36:11 PM
+SELECT
+    DISTINCT L1.num AS ConsecutiveNums
+FROM Logs L1, Logs L2, Logs L3
+WHERE L1.num = L2.num
+  AND L2.num = L3.num
+  AND L1.id = L2.id - 1
+  AND L2.id = L3.id - 1;
