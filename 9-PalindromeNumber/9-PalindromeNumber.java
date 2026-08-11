7b64baaa26d6b70f8dec457318e35893072585c9
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-08-11 18:38:32 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 8/11/2026, 6:38:31 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers and multiples of 10 (except 0) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x /= 10;
+        }
+
+        // For even length numbers: x == reversedHalf
+        // For odd length numbers: x == reversedHalf/10
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
