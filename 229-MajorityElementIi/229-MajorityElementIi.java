dca9a56ef8ee7d0d92e16e2ebd6658b6aedf0ab0
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-08-11 18:35:13 +0530

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.java
@@ -0,0 +1,16 @@
+// Last updated: 8/11/2026, 6:35:12 PM
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n , map.getOrDefault(n , 0 ) + 1);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int limit = nums.length/3;
+
+        for(int k : map.keySet())   if(map.get(k) > limit) result.add(k);
+        
+        return result;
+    }
+}
