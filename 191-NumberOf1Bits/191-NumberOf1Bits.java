6d3cb7d2df58b724bb640ba5c9d436df5c29bf1b
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-08-11 18:35:56 +0530

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.java
@@ -0,0 +1,12 @@
+// Last updated: 8/11/2026, 6:35:55 PM
+class Solution {
+    public int hammingWeight(int n) {
+        int c=0;
+        while(n!=0)
+        {
+            n=n&n-1;
+            c++;
+        }
+        return c;
+    }
+}
