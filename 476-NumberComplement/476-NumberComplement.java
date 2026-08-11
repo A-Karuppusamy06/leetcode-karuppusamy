e96e32017352f957de38d866b8baa3831b4cf498
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-08-11 18:33:58 +0530

    Create 476-NumberComplement

---
+++ 476-NumberComplement/476-NumberComplement.java
@@ -0,0 +1,13 @@
+// Last updated: 8/11/2026, 6:33:57 PM
+class Solution {
+    public int findComplement(int num) {
+        int mask=0;
+        int temp=num;;
+        while(temp>0)
+        {
+            mask=(mask<<1)|1;
+            temp>>=1;
+        }
+        return num^mask;
+    }
+}
