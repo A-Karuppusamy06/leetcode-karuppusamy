54cd3a47bf8eb0052620b372dbba91936f468a62
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-08-11 18:33:13 +0530

    Create 789-KthLargestElementInAStream

---
+++ 789-KthLargestElementInAStream/789-KthLargestElementInAStream.java
@@ -0,0 +1,22 @@
+// Last updated: 8/11/2026, 6:33:12 PM
+   class KthLargest {
+        final PriorityQueue<Integer> q;
+        final int k;
+
+        public KthLargest(int k, int[] a) {
+            this.k = k;
+            q = new PriorityQueue<>(k);
+            for (int n : a)
+                add(n);
+        }
+
+        public int add(int n) {
+            if (q.size() < k)
+                q.offer(n);
+            else if (q.peek() < n) {
+                q.poll();
+                q.offer(n);
+            }
+            return q.peek();
+        }
+    }
