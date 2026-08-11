368ec026d92db5e8019e64370d2c459d52c9176b
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-08-11 18:37:45 +0530

    Create 51-NQueens

---
+++ 51-NQueens/51-NQueens.java
@@ -0,0 +1,48 @@
+// Last updated: 8/11/2026, 6:37:44 PM
+class Solution {
+    List<List<String>> ans = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++)
+            Arrays.fill(board[i], '.');
+
+        solve(0, board, n);
+        return ans;
+    }
+
+    void solve(int row, char[][] board, int n) {
+        if (row == n) {
+            List<String> temp = new ArrayList<>();
+            for (char[] r : board)
+                temp.add(new String(r));
+            ans.add(temp);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+                solve(row + 1, board, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    boolean isSafe(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q')
+                return false;
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q')
+                return false;
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q')
+                return false;
+
+        return true;
+    }
+}
