4ce02a9ccde3b1a706050ad4ce5dfcf2ada952b3
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-09-11 09:25:16 +0530

    Create 713-SubarrayProductLessThanK

---
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -0,0 +1,22 @@
+// Last updated: 9/11/2026, 9:25:14 AM
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int n = nums.length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+
+            for (int j = i; j < n; j++) {
+                product *= nums[j];
+
+                if (product < k) {
+                    ans++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
