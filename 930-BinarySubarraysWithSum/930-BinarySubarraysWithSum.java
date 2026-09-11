229e122213ad2ab6d1e156b775e4fb5139e5bb91
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-09-11 09:24:22 +0530

    Create 930-BinarySubarraysWithSum

---
+++ 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.java
@@ -0,0 +1,18 @@
+// Last updated: 9/11/2026, 9:24:21 AM
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+    private int atMost(int[] nums, int goal) {
+        int head, tail = 0, sum = 0, result = 0;
+        for (head = 0; head < nums.length; head++) {
+            sum += nums[head];
+            while (sum > goal && tail <= head) {
+                sum -= nums[tail];
+                tail++;
+            }
+            result += head - tail + 1;
+        }
+        return result;
+    }
+}
