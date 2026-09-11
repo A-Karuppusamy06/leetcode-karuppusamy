a98bff42b3d57787f6c53e8404d99c2859dadb55
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-09-11 09:22:55 +0530

    Create 1952-ThreeDivisors

---
+++ 1952-ThreeDivisors/1952-ThreeDivisors.java
@@ -0,0 +1,16 @@
+// Last updated: 9/11/2026, 9:22:54 AM
+class Solution {
+    public boolean isThree(int n) {
+        int root = (int)Math.sqrt(n);
+        if (root * root != n) return false; // not a perfect square
+        return isPrime(root);
+    }
+
+    private boolean isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+}
