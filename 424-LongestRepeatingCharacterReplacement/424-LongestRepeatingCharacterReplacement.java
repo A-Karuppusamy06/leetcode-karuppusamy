7f14d61fc6a5b41f3f85dc5138ca5e7a599401ae
KARUPPUSAMY A <karuppusamyashok@gmail.com>
2026-09-11 09:25:51 +0530

    Create 424-LongestRepeatingCharacterReplacement

---
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,17 @@
+// Last updated: 9/11/2026, 9:25:50 AM
+class Solution {
+       public int characterReplacement(String s, int k) {
+        int len = s.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+        for (int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+}
